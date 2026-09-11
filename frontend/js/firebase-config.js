d4abd932b0c9be3d67e027dcf3b13f5ee8d0add6
gbemigaakinde <gbemigaakinde@gmail.com>
2026-09-11 09:57:15 +0100

    Add Firebase configuration for Fahmid School project
    This file contains the Firebase web configuration for the Fahmid School Management System, including API keys and project identifiers.

---
+++ frontend/js/firebase-config.js
@@ -0,0 +1,33 @@
+/**
+ * FAHMID SCHOOL MANAGEMENT SYSTEM
+ * Firebase Web Configuration
+ *
+ * This configuration belongs to the existing Fahmid Firebase project.
+ *
+ * IMPORTANT:
+ * Firebase Web configuration is not a service-account credential.
+ * Never place service-account private keys here.
+ */
+
+export const firebaseConfig = {
+  apiKey:
+    "AIzaSyD0A1zJ9bGCwk_UoibgsNWrV2M9C51aDo",
+
+  authDomain:
+    "fahmid-school.firebaseapp.com",
+
+  projectId:
+    "fahmid-school",
+
+  storageBucket:
+    "fahmid-school.firebasestorage.app",
+
+  messagingSenderId:
+    "48604608508",
+
+  appId:
+    "1:48604608508:web:5b387a2de260b9851a6479",
+
+  measurementId:
+    "G-HEC84JXFY2",
+};
